4373b585501907151e81af5c7e852a3095832083
lucasbedout <lucas.bedout@free.fr>
2013-09-28 11:03:40 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lesxviezvous::Application.config.secret_token = 'fedb0ecaba8f2730b92c26ec4730cd53e4e81c26662afc01c34eebed7e1238113977e0522e5b3e3ea6c41e6d5ee5bdf56d396a2ddf53980525a06575ad95c3d3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Lesxviezvous::Application.config.session_store :cookie_store, key: '_lesxviezvous_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Lesxviezvous::Application.config.session_store :active_record_store
