ae1feb069b8177a572e93cfea7f22952bf951cab
lucasbedout <lucas.bedout@free.fr>
2013-10-11 14:24:54 +0000

    removed title column in posts

--- db/migrate/20130928200415_create_posts.rb
+++ db/migrate/20130928200415_create_posts.rb
@@ -1,14 +1,13 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.string :title
       t.text :content
-      t.integer :upvotes
-      t.integer :downvotes
-      t.integer :fakevotes
+      t.integer :upvotes, :default => 0
+      t.integer :downvotes, :default => 0
+      t.integer :fakevotes, :default => 0
       t.integer :user_id
       t.integer :category_id
-      t.integer :status
+      t.integer :status, :default => 0
       t.string :source
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -21,17 +21,16 @@ ActiveRecord::Schema.define(:version => 20130928200415) do
   end
 
   create_table "posts", :force => true do |t|
-    t.string   "title"
     t.text     "content"
-    t.integer  "upvotes"
-    t.integer  "downvotes"
-    t.integer  "fakevotes"
+    t.integer  "upvotes",     :default => 0
+    t.integer  "downvotes",   :default => 0
+    t.integer  "fakevotes",   :default => 0
     t.integer  "user_id"
     t.integer  "category_id"
-    t.integer  "status"
+    t.integer  "status",      :default => 0
     t.string   "source"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
   end
 
 end
