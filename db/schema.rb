ebe1b400fe6e65a1630dde2c24b80e6ca7fe2503
lucasbedout <lucas.bedout@free.fr>
2013-10-26 22:59:13 +0000

    added profile picture for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131015124759) do
+ActiveRecord::Schema.define(:version => 20131026225748) do
 
   create_table "admin_users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(:version => 20131015124759) do
     t.integer  "rank",            :default => 0
     t.datetime "created_at",                            :null => false
     t.datetime "updated_at",                            :null => false
+    t.string   "picture"
   end
 
   create_table "posts", :force => true do |t|
@@ -73,6 +74,7 @@ ActiveRecord::Schema.define(:version => 20131015124759) do
     t.integer  "rank",             :default => 0
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "picture"
   end
 
 end
