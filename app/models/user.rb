e402608911f140aca4a5b5ac0ce16d1a547474f7
lucasbedout <lucas.bedout@free.fr>
2013-10-29 17:08:41 +0000

    prepare social features

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :oauth_expires_at, :oauth_token, :provider, :uid
+  has_many :posts
 
   def self.from_omniauth(auth)
     find_by_provider_and_uid(auth["provider"], auth["uid"]) || create_with_omniauth(auth)
