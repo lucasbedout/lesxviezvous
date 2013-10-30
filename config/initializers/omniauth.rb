ead379ad077a5f5916ea1b278bd70d33fcd5d878
lucasbedout <lucas.bedout@free.fr>
2013-10-30 01:09:40 +0000

    corrected issue on timeline

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,6 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '490620914369032', 'e37824282407bcf8911605893867df6a'
-  provider :twitter, 'JBFwbA8tarcyhfzYKqdaw', 'JCtoUfYJlYb0IxUSgEtijufEinhrVftHZcYFlG1Sw'
+  provider :twitter, 'JBFwbA8tarcyhfzYKqdaw', 'JCtoUfYJlYb0IxUSgEtijufEinhrVftHZcYFlG1Sw',
   provider :identity
 end
