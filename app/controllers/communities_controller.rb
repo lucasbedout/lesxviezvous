23f2a8cb451d1924cdb28edd4dbe82814fdfddb7
lucasbedout <lucas.bedout@free.fr>
2013-11-06 08:08:56 +0000

    community edit

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -24,9 +24,26 @@ class CommunitiesController < ApplicationController
 	        format.json { render json: @community.errors, status: :unprocessable_entity }
 	      end
 	    end
-	  end
+	end
 
-	  def join 
+	def edit
+		@community = Community.find(params[:id])
+	end
+
+	def update
+	    @community = Community.find(params[:id])
+		    respond_to do |format|
+		      if @community.update_attributes(params[:community])
+		        format.html { redirect_to @community, notice: 'community was successfully updated.' }
+		        format.json { head :no_content }
+		      else
+		        format.html { render action: "edit" }
+		        format.json { render json: @community.errors, status: :unprocessable_entity }
+		      end
+		    end
+	end
+
+	 def join 
 	  	@community = Community.find(params[:community_id])
 
 	  	if @community.public == true
@@ -39,9 +56,9 @@ class CommunitiesController < ApplicationController
 	        format.html { redirect_to @community, notice: 'Merci de nous avoir rejoint' }
 	        format.json { render json: @community, status: :created, location: @community }
 	    end
-	  end
+	end
 
-	 def leave 
+	def leave 
 	  	@community = Community.find(params[:community_id])
 	  	 respond_to do |format|
 	  	 	current_user.leave @community
--- app/models/community.rb
+++ app/models/community.rb
@@ -1,5 +1,5 @@
 class Community < ActiveRecord::Base
-  attr_accessible :name, :owner_id, :public, :category_id
+  attr_accessible :name, :owner_id, :public, :category_id, :picture, :place, :description
   belongs_to :category
   acts_as_inkwell_community
 end
